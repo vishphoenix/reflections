d9ccb36f0efaf5218d0792767d2d648337594fd5
Vish <48857573+vishphoenix@users.noreply.github.com>
2020-08-25 21:38:16 +0530

    Delete admin.py

--- users/basicapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from basicapp.models import UserProfileInfo,members,ActivityPeriod
-
-# Register your models here.
-admin.site.register(UserProfileInfo)
-admin.site.register(members)
-admin.site.register(ActivityPeriod)
